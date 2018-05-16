c126c2f5022b1e79593ef6a03f3870d676417175
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-16 17:19:58 +1000

    Setting up the structur of the app

---
+++ src/routes/topics/index.js
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+// import { Link } from 'preact-router/match';
+// import style from './style';
+
+export default function topics() {
+  return (
+    <ul>
+        <li>
+          <span>1</span>
+          <a href={`/`}>Title</a>
+          <a href={`/`}>14</a>
+          <span>1212 points by john_dov 1h</span>
+        </li>
+    </ul>
+  );
+}
