b3eeab663c7094690d42344aaf7dec43c94e11e2
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-16 17:20:27 +1000

    Setting up the structur of the app

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import { Router } from 'preact-router';
 import Header from './header';
 import Home from '../routes/home';
 import Profile from '../routes/profile';
+import TopicView from '../components/topics';
 // import Home from 'async!../routes/home';
 // import Profile from 'async!../routes/profile';
 
@@ -12,6 +13,7 @@ if (module.hot) {
 }
 
 export default class App extends Component {
+
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
