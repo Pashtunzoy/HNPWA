702bd75fe17a71505b7f66379cada614a02f851c
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-19 16:35:57 +1000

    Added a HN logo and text using text

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,15 +6,16 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>HNPWA</h1>
+				<span class={style.logo}>HN</span>
+        <span class={style.brand}>Hacker News</span>
 				<nav>
 					<Link activeClassName={style.active} href="/">Top</Link>
 					<Link activeClassName={style.active} href="/newest">New</Link>
 					<Link activeClassName={style.active} href="/show">Show</Link>
 					<Link activeClassName={style.active} href="/ask">Ask</Link>
 					<Link activeClassName={style.active} href="/jobs">Jobs</Link>
-					<Link activeClassName={style.active} href="/profile">Me</Link>
-					<Link activeClassName={style.active} href="/profile/john">John</Link>
+					{/* <Link activeClassName={style.active} href="/profile">Me</Link>
+					<Link activeClassName={style.active} href="/profile/john">John</Link> */}
 				</nav>
 			</header>
 		);
