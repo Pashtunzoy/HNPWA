8b0614da580235e0af9b2651325b9ed1bc4cb66d
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-19 16:41:51 +1000

    Added the barebone of comments section of the app

---
+++ src/components/topics/comments/index.js
@@ -0,0 +1,17 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class Comments extends Component {
+
+	fetchComments(id) {
+		return;
+	}
+
+	render() {
+		return (
+			<section>
+				<h1>Comments</h1>
+			</section>
+		);
+	}
+}
