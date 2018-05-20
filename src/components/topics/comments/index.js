6e0f92ee476eb440e29c87e8c1081fae09dc8062
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-20 22:34:49 +1000

    Added the comments list to the comment component

--- src/components/topics/comments/index.js
+++ src/components/topics/comments/index.js
@@ -3,15 +3,37 @@ import style from './style';
 
 export default class Comments extends Component {
 
-	fetchComments(id) {
-		return;
-	}
+  state = {
+  	topic: {}
+  };
 
-	render() {
-		return (
-			<section>
-				<h1>Comments</h1>
-			</section>
-		);
-	}
+  fetchTopic() {
+  	fetch(`https://node-hnapi.herokuapp.com${window.location.pathname}`)
+  		.then(res => res.json())
+  		.then(data => {
+  			this.setState({ topic: data });
+  		});
+  }
+  
+  componentDidMount() {
+  	this.fetchTopic();
+  }
+
+  render() {
+  	const { topic } = this.state;
+  	const { title, domain, user, time_ago, comments } = topic;
+  	return (
+  		<section>
+  			<h1>{title}({domain})</h1>
+  			<span>{user}</span>
+  			<span>{time_ago}</span>
+  			<ul>
+  				{comments && comments.map(comment => (<li>
+  					{comment.content}
+  				</li>
+  				))}
+  			</ul>
+  		</section>
+  	);
+  }
 }
