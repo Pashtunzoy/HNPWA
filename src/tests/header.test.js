4d21ec1b15dfbb425c60c4cd5a538f0984e00271
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-28 22:10:38 +1000

    Added generic test to the header test file

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -5,10 +5,10 @@ import { Link } from 'preact-router/match';
 import { shallow, deep } from 'preact-render-spy';
 
 describe('Initial Test of the Header', () => {
-
     test('Header renders 3 nav items', () => {
         const context = shallow(<Header />);
-        expect(context.find('h1').text()).toBe('Preact App');
-        expect(context.find(<Link />).length).toBe(3);
+        expect(context.find('span')[0].text()).toBe('HN');
+        expect(context.find('span')[1].text()).toBe('Hacker News');
+        expect(context.find(<Link />).length).toBe(5);
     });
 });
