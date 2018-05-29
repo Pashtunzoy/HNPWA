36ba344c04b416d31a5ad2e600ed576f57170862
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-29 22:40:53 +1000

    Fixed the import and also added the isomorphic fetch

--- src/tests/comments.test.js
+++ src/tests/comments.test.js
@@ -1,11 +1,11 @@
 import { h, Component } from 'preact';
-import Comments from '../components/comments';
+import 'isomorphic-fetch';
+import Comments from '../components/topics/comments';
 import { Link } from 'preact-router/match';
 // See: https://github.com/mzgoddard/preact-render-spy
 import { shallow, deep } from 'preact-render-spy';
 
 describe('Initial Test of the Comments', () => {
-
     test('Comments renders lists', () => {
         const context = shallow(<Comments />);
         // expect(context.find('h1').text()).toBe('Preact App');
